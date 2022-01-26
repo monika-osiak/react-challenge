56d1b169c0b10ae93b9d8e1b993b289df248e350
Monika Osiak <m.osiak46@gmail.com>
2022-01-26 21:15:12 +0100

    Very first version of NoContent

--- client/src/ui/atoms/NoContent.jsx
+++ client/src/ui/atoms/NoContent.jsx
@@ -1,3 +1,10 @@
+import logo from '../../assets/no_content.png';
+
 export const NoContent = () => {
-  return null;
+  return (
+    <div>
+      <img style={{display: "block", "margin-left": "auto", "margin-right": "auto"}} src={logo}/>
+      <p style={{ color: "#333333", opacity: 0.5, "text-align": "center" }}>Brak danych do wyświetlenia</p>
+    </div>
+  );
 };
