c544193b3a0f25d3bebddda45300b17ee9939808
Monika Osiak <m.osiak46@gmail.com>
2022-01-27 21:46:49 +0100

    Style hover

--- client/src/ui/atoms/Button.jsx
+++ client/src/ui/atoms/Button.jsx
@@ -42,32 +42,63 @@ myTheme = createTheme(myTheme, {
     MuiButton: {
       styleOverrides: {
         containedPrimary: {
-          backgroundColor: myTheme.palette.primary.main
+          backgroundColor: myTheme.palette.primary.main,
+          '&:hover': {
+            backgroundColor: myTheme.palette.primary.dark
+          }
         },
         outlinedPrimary: {
           backgroundColor: myTheme.palette.secondary.main,
-          borderStyle: "none"
+          borderStyle: "none",
+          '&:hover': {
+            backgroundColor: myTheme.palette.secondary.dark,
+            borderStyle: "none"
+          }
         },
         containedError: {
           color: myTheme.palette.error.main,
-          backgroundColor: myTheme.palette.error.light
+          backgroundColor: myTheme.palette.error.light,
+          '&:hover': {
+            backgroundColor: myTheme.palette.error.main,
+            color: myTheme.palette.grey.background
+          },
+
         },
         outlinedError: {
-          backgroundColor: myTheme.palette.grey.background
+          backgroundColor: myTheme.palette.grey.background,
+          '&:hover': {
+            backgroundColor: myTheme.palette.error.light
+          }
         },
         containedSuccess: {
           backgroundColor: myTheme.palette.success.light,
-          color: myTheme.palette.success.dark
+          color: myTheme.palette.success.dark,
+          '&:hover': {
+            backgroundColor: myTheme.palette.success.dark,
+            color: myTheme.palette.grey.background
+          }
         },
         outlinedSuccess: {
-          backgroundColor: myTheme.palette.grey.background
+          backgroundColor: myTheme.palette.grey.background,
+          borderStyle: "1px solid myTheme.palette.success.main",
+          '&:hover': {
+            borderStyle: "1px solid myTheme.palette.success.main"
+          }
         },
         containedWarning: {
           backgroundColor: myTheme.palette.warning.light,
-          color: myTheme.palette.warning.dark
+          color: myTheme.palette.warning.dark,
+          '&:hover': {
+            backgroundColor: myTheme.palette.warning.main,
+            color: myTheme.palette.grey.background
+          }
         },
         outlinedWarning: {
-          backgroundColor: myTheme.palette.grey.background
+          backgroundColor: myTheme.palette.grey.background,
+          '&:hover': {
+            backgroundColor: myTheme.palette.warning.light,
+            borderStyle: "1px solid myTheme.palette.warning.main"
+          }
         },
       }
     }
