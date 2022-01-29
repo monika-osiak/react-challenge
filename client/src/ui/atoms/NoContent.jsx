a03344e6d6642a951e0ce3d9a8ae0c9d95afb1d8
Monika Osiak <m.osiak46@gmail.com>
2022-01-29 21:32:43 +0100

    Style NoContent atom

--- client/src/ui/atoms/NoContent.jsx
+++ client/src/ui/atoms/NoContent.jsx
@@ -1,10 +1,35 @@
+import { Box } from '@mui/material';
 import logo from '../../assets/no_content.png';
+import '../styles.css';
 
 export const NoContent = () => {
   return (
-    <div>
-      <img style={{display: "block", "margin-left": "auto", "margin-right": "auto"}} src={logo}/>
-      <p style={{ color: "#333333", opacity: 0.5, "text-align": "center" }}>Brak danych do wyświetlenia</p>
-    </div>
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      <div>
+        <img style={{
+          display: 'block',
+          marginLeft: 'auto',
+          marginRight: 'auto'
+        }} src={logo}/>
+        <p style={{
+          color: '#333333',
+          opacity: 0.5,
+          textAlign: 'center',
+          fontFamily: 'Inter',
+          fontStyle: 'normal',
+          fontWeight: 'normal',
+          fontSize: '19.2px',
+          lineHeight: '29px',
+          marginTop: 0
+        }}>Brak danych do wyświetlenia</p>
+      </div>
+    </Box>
   );
 };
